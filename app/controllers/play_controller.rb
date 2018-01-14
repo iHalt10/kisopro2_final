352016b501efbff25c7a18f30d645b7734b02432
Takeaki Oura <rtm6@me.com>
2018-01-14 23:26:51 +0900

    Edit play controller Cancel request

--- app/controllers/play_controller.rb
+++ app/controllers/play_controller.rb
@@ -19,19 +19,24 @@ class PlayController < ApplicationController
       turnState = Redis.current.hget( "MatchUp:" + params["key"], "turnState").to_i
       if params["Iam"] == "DF"
         # DF 奇数か調べる 偶数だと更新されていない
-        if turnState.odd?
-          turnInfo = Redis.current.hget( "MatchUp:" + params["key"], "turnInfo")
-          response = "YES," + turnInfo
-        else
-          response = "NO"
+        response = "YES,Cancel"
+        if Redis.current.type( "MatchUp:" + params["key"] + ":Cancel:PF") == "none"
+          if turnState.odd?
+            turnInfo = Redis.current.hget( "MatchUp:" + params["key"], "turnInfo")
+            response = "YES," + turnInfo
+          else
+            response = "NO"
+          end
         end
       else # params["Iam"] == "PF"
         # PF 偶数か調べる 奇数だと更新されていない
-        if turnState.even?
-          turnInfo = Redis.current.hget( "MatchUp:" + params["key"], "turnInfo")
-          response = "YES," + turnInfo
-        else
-          response = "NO"
+        if Redis.current.type( "MatchUp:" + params["key"] + ":Cancel:DF") == "none"
+          if turnState.even?
+            turnInfo = Redis.current.hget( "MatchUp:" + params["key"], "turnInfo")
+            response = "YES," + turnInfo
+          else
+            response = "NO"
+          end
         end
       end
     end
@@ -39,10 +44,12 @@ class PlayController < ApplicationController
   end
 
   def cancel
-    if params["key"].nil?
+    if params["key"].nil? || params["Iam"].nil?
       response = "error:NotParam"
     else
-      Redis.current.hmset( "MatchUp:" + params["key"], "turnInfo", "Cancel" )
+      Redis.current.set("MatchUp:" + params["key"] + ":Cancel:" + params["Iam"], "")
+      Redis.current.expire("MatchUp:" + params["key"] + ":Cancel:" + params["Iam"], 6.hour )
+      # Redis.current.hmset( "MatchUp:" + params["key"], "turnInfo", "Cancel" )
       response = "Cancel"
     end
     render :text => response
